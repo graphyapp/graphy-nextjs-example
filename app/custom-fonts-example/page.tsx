a1e2506913055b6f437de7b4c3d93510b8339a4b
jaume-graphy <jaume@graphyapp.com>
2025-10-15 10:41:39 +0200

    chore: add new example page

---
+++ app/custom-fonts-example/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import {SPEND_BREAKDOWN} from "@/src/datasets/spend-breakdown";
+import {ReturnHome} from "@/src/ReturnHome";
+import {
+  buildChartTitleDocument,
+  CustomPaletteCatalog,
+  Graph,
+  GraphConfig,
+  GraphProvider,
+} from "@graphysdk/core";
+import {useEffect, useState} from "react";
+
+export default function CustomFontsExample() {
+  // Note: Currently graphySDK is not working with SSR.
+  const [isMounted, setIsMounted] = useState(false);
+  const [config, setConfig] = useState<GraphConfig>(initialConfig);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet"/>
+      <ReturnHome/>
+      <GraphProvider
+        config={config}
+        onChange={(update) => {
+          setConfig((currentValues) => ({...currentValues, ...update}));
+        }}
+        theme="light"
+      >
+        <div className="flex p-5 max-w-6xl mx-auto items-center justify-center">
+            <Graph isEditable={false} />
+          </div>
+      </GraphProvider>
+    </>
+  );
+}
+
+const initialConfig: GraphConfig = {
+  data: SPEND_BREAKDOWN.data,
+  datasetConfig: SPEND_BREAKDOWN.config,
+  visualisationConfig: {
+    type: "pie",
+    pieAppearance: "donut",
+    legendPosition: "right",
+  },
+  customAppearanceConfig: {
+    theme: "graphy",
+    palette: "graphy",
+    background: "LIGHT",
+    borderStyle: "none",
+    borderStyleName: null,
+    backgroundStyle: "transparent",
+    textScale: 1.4,
+    font: {
+      heading: {
+        fontId: "primary",
+        color: "#1f57d3"
+      },
+      body: {
+        fontId: "secondary",
+        color: "#3a94ed"
+      }
+    }
+  },
+  titleDocument: buildChartTitleDocument({
+    title: "Custom font families and colors",
+  }),
+  fontList: [
+    {
+      id: "primary",
+      label: "Heading",
+      fontFamily: "'Nova Square', 'Georgia', 'Times New Roman', serif", //LOADED FROM GOOGLE FONTS
+    },
+    {
+      id: "secondary",
+      label: "Body",
+      fontFamily: "'Comic Sans MS', 'Comic Sans', 'Chalkboard SE', 'Comic Neue', cursive", //SYSTEM FONTS
+    },
+  ]
+};
